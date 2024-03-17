ddc8bb0b34a41f285873d2a4e5034ecb7859164f
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2024-03-17 20:40:12 +0900

    fix : fix roof disable bug
    fix roof disable bug

--- Project/Script/ER_DataScript_LandMeshBase.cpp
+++ Project/Script/ER_DataScript_LandMeshBase.cpp
@@ -3,6 +3,8 @@
 
 #include <Engine\CResMgr.h>
 
+int ER_DataScript_LandMeshBase::m_iRoofCount = 0;
+
 ER_DataScript_LandMeshBase::ER_DataScript_LandMeshBase()
 	: CScript((UINT)SCRIPT_TYPE::ER_DATASCRIPT_LANDMESHBASE)
 {
@@ -25,20 +27,24 @@ void ER_DataScript_LandMeshBase::tick()
 }
 
 void ER_DataScript_LandMeshBase::BeginOverlap(CCollider3D* _Other)
-{
+{	
 	if (_Other->GetOwner()->GetLayerIndex() == 13) {
+		++m_iRoofCount;
 		m_pRoof->SetEnable(false);
 	}
 }
 
 void ER_DataScript_LandMeshBase::OnOverlap(CCollider3D* _Other)
 {
+	
 }
 
 void ER_DataScript_LandMeshBase::EndOverlap(CCollider3D* _Other)
 {
 	if (_Other->GetOwner()->GetLayerIndex() == 13) {
-		m_pRoof->SetEnable(true);
+		--m_iRoofCount;
+		if(m_iRoofCount == 0)
+			m_pRoof->SetEnable(true);
 	}
 }
 
