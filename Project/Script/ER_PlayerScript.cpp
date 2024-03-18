6681b4ed0373b0abaccbe1862f7c97fcc268bab6
660C <96910727+chnxik@users.noreply.github.com>
2024-03-18 11:58:36 +0900

    Player Script 구조 범용성증가 수정

--- Project/Script/ER_PlayerScript.cpp
+++ Project/Script/ER_PlayerScript.cpp
@@ -4,6 +4,7 @@
 #include <Engine\CPathFindMgr.h>
 #include <Engine\CRenderMgr.h>
 #include <Engine\CCollisionMgr.h>
+#include <Engine\CUIMgr.h>
 
 #include <Engine\CFindPath.h>
 
@@ -36,10 +37,16 @@ void ER_PlayerScript::tick()
 {
 	// [ Mouse ]
 
-	CGameObject* pFocusObj = GetFocusObj();	// 타겟 오브젝트
-	Vec3 vTargetPoint = GetFocusPoint();	// 타겟 지점
+	CGameObject* pTargetObj = GetFocusObj();	// 타겟 오브젝트
+	Vec3 vCsrPoint = GetFocusPoint();	// 타겟 지점
 
 	tFSMData data = {};
+
+	// v4Data : 마우스 위치, 마우스 방향 활용가능
+	// lParam : 현재 마우스위치에 충돌중인 오브젝트
+
+	data.v4Data = vCsrPoint;
+	data.lParam = (DWORD_PTR)pTargetObj;
 
 	// [ Mouse Control ]
 	// 이동
@@ -50,38 +57,61 @@ void ER_PlayerScript::tick()
 		// 현재 충돌중인 오브젝트
 		// Get Cur Collision CGameObject Type 값
 
+		// 공격대기상태라면 해제
+
 		// cursor On UI
-		if(false)
-		{ }
+		CGameObject* FocusedUI = CUIMgr::GetInst()->GetFocusedUI();
+		if(FocusedUI)
+		{ 
+			// UI는 UIMgr로 확인하기때문에 UIMgr에서 Focused UI가 있는지 판단.
+			// UI는 UI에서 판단하기때문에 동작하지 않고 넘어간다.
+
+			// 플레이어스크립트에서는 별도 동작 하지 않음
+		}
 		// cursor On Enemy
 		else if(false)
-		{ }
+		{
+			// false에 있는 조건에 data.lParam(pTargetObj)가 Enemy인지 Box인지 판단할 수 있는 참거짓 값 입력
+		}
 		// cursor On Box
 		else if(false)
-		{ }
+		{
+			// false에 있는 조건에 data.lParam(pTargetObj)가 Enemy인지 Box인지 판단할 수 있는 참거짓 값 입력
+		}
 		// cursor On Land (else)
 		else
 		{
-			data.v4Data = vTargetPoint;
 			m_pActionScript->Move(data);
 		}
 	}
 	else if (KEY_PRESSED(KEY::RBTN))
 	{
-		// 이동 유지
-		data.v4Data = vTargetPoint;
 		m_pActionScript->Move(data);
 	}
 	
 	if (KEY_TAP(KEY::A))
 	{
 		// 공격
-		// 공격 상태가 아닌경우 공격명령대기상태
-
-		// 공격명령 상태라면
+		// 공격 상태가 아닌경우 공격대기상태설정
+		// 공격대기상태는 마우스커서변경만 영향
 	}
 
 
+	if (KEY_TAP(KEY::LBTN))
+	{
+		// KEY_TAP 앞에 공격명령상태 판단 참거짓 값 추가하여 (true && KEY_TAP(KEY::LBTN)) 으로 조건 생성
+		// 공격대기상태 해제
+		// pTargetObj가 Enemy인 경우 Attack 명령 실행
+		if (false)
+		{
+			m_pActionScript->Attack(data);
+		}
+	}
+
+
+	// v4Data : 마우스 위치, 마우스 방향 활용가능
+	// lParam : 현재 마우스위치에 충돌중인 오브젝트
+	
 	if (KEY_TAP(KEY::Q))
 	{
 		m_pActionScript->Skill_Q(data);
@@ -92,7 +122,6 @@ void ER_PlayerScript::tick()
 	}
 	if (KEY_TAP(KEY::E))
 	{
-		data.v4Data = vTargetPoint;
 		m_pActionScript->Skill_E(data);
 	}
 	if (KEY_TAP(KEY::R))
@@ -129,13 +158,14 @@ void ER_PlayerScript::tick()
 
 
 
-	// Camera Pixed this Obj
+	// 카메라 시야 캐릭터로 고정
 	if (KEY_TAP(KEY::Y))
 	{
 		CGameObject* pMainCam = CRenderMgr::GetInst()->GetMainCam()->GetOwner();
 		pMainCam->GetScript<ER_CamControllerScript>()->CameraFixToggle();
 	}
 
+	// SPACE 바 누르는동안 메인카메라가 캐릭터를 추적한다.
 	if (KEY_PRESSED(KEY::SPACE))
 	{
 		CGameObject* pMainCam = CRenderMgr::GetInst()->GetMainCam()->GetOwner();
@@ -161,6 +191,7 @@ Vec3 ER_PlayerScript::GetFocusPoint()
 
 CGameObject* ER_PlayerScript::GetFocusObj()
 {
+	// 마우스 레이와 겹쳐진 오브젝트 판단
 	return nullptr;
 }
 
